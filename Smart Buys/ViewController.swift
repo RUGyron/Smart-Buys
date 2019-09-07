0c600186482f153de0fdf5f9590829f1fb3f85f5
Vladislav <pivosh098@gmail.com>
2019-09-08 00:00:11 +0300

    Initial Commit

---
+++ Smart Buys/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart Buys
+//
+//  Created by ipopat on 08/09/2019.
+//  Copyright © 2019 rugyron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
